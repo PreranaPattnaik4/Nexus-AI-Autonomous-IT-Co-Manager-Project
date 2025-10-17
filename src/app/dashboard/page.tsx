f396c6ea587414b349dc9e26b11f678c279b1310
Prerana Kailash Pattnaik <preranapattnaik4@gmail.com>
2025-10-17 15:29:48 +0000

    make "System Health and AI Insights Weekly optimization summary "section

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,16 +14,16 @@ export default function DashboardPage() {
         <TaskStats />
       </div>
 
-      <div className="col-span-1 lg:col-span-5 grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="lg:col-span-2 grid grid-cols-1 gap-6">
         <SystemHealthSummary />
         <AiInsightsCard />
       </div>
       
-      <div className="col-span-1 lg:col-span-5 grid grid-cols-1 lg:grid-cols-5 gap-6">
-        <div className="lg:col-span-2">
+      <div className="col-span-1 lg:col-span-3 grid grid-cols-1 lg:grid-cols-1 gap-6">
+        <div className="lg:col-span-1">
             <GoalForm />
         </div>
-        <div className="lg:col-span-3">
+        <div className="lg:col-span-1">
             <GeminiThinkingMap />
         </div>
       </div>
