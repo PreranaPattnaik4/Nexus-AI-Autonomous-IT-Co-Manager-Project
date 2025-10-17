d97e0db1ca21d310548c08b8da89b3767c8afe95
Prerana Kailash Pattnaik <preranapattnaik4@gmail.com>
2025-10-17 15:28:19 +0000

    Goal-Based Task Management and Multi-Agent Collaboration section move by

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,22 +13,29 @@ export default function DashboardPage() {
       <div className="col-span-1 lg:col-span-5">
         <TaskStats />
       </div>
-      <div className="col-span-1 lg:col-span-3 grid grid-cols-1 md:grid-cols-2 gap-6">
+
+      <div className="col-span-1 lg:col-span-5 grid grid-cols-1 md:grid-cols-2 gap-6">
         <SystemHealthSummary />
         <AiInsightsCard />
       </div>
-      <div className="col-span-1 lg:col-span-2">
+      
+      <div className="col-span-1 lg:col-span-5 grid grid-cols-1 lg:grid-cols-5 gap-6">
+        <div className="lg:col-span-2">
+            <GoalForm />
+        </div>
+        <div className="lg:col-span-3">
+            <GeminiThinkingMap />
+        </div>
+      </div>
+
+      <div className="col-span-1 lg:col-span-5">
         <AlertsCard />
       </div>
-      <div className="col-span-1 lg:col-span-2">
-        <GoalForm />
-      </div>
-       <div className="col-span-1 lg:col-span-3">
-        <GeminiThinkingMap />
-      </div>
+
       <div className="col-span-1 lg:col-span-5">
         <TasksList />
       </div>
+
       <div className="col-span-1 lg:col-span-5">
         <MetricsCharts />
       </div>
