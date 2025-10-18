239facc152b7b7a5a4ba9384a3fbeee415605948
Prerana Kailash Pattnaik <preranapattnaik4@gmail.com>
2025-10-18 17:47:53 +0000

    MAKE THE HEADER TRANPARENT

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -34,7 +34,7 @@ export function AppHeader() {
   const notifications = initialAlerts;
 
   return (
-    <header className="flex h-16 items-center border-b bg-card px-6">
+    <header className="flex h-16 items-center px-6">
       <div className="relative flex-1">
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
         <Input
