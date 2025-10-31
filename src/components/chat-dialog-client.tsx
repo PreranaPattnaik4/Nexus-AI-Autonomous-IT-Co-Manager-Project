694619367b3fe110695c77cf9167c74c4a1f9d87
Prerana Kailash Pattnaik <preranapattnaik4@gmail.com>
2025-10-31 06:00:59 +0000

    in chatbot page the enter button hide behind the chatbot button

--- src/components/chat-dialog-client.tsx
+++ src/components/chat-dialog-client.tsx
@@ -1,11 +1,20 @@
+
 'use client';
 
 import dynamic from 'next/dynamic';
+import { usePathname } from 'next/navigation';
 
 const ChatDialog = dynamic(() => import('./chat-dialog').then(mod => mod.ChatDialog), {
     ssr: false,
 });
 
 export default function ChatDialogClient() {
+    const pathname = usePathname();
+
+    // Do not render the floating chat dialog button on the dedicated chat page
+    if (pathname === '/chat') {
+        return null;
+    }
+
     return <ChatDialog />;
 }
